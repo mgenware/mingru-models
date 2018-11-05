e043d2f612e493cde63c586b6c029c242f7f6374
Mgen <m@mgenware.com>
2018-11-06 02:52:20 +0800

    Update select tests

--- tests/views/select.test.ts
+++ tests/views/select.test.ts
@@ -2,7 +2,11 @@ import * as dd from '../..';
 import user from '../models/user';
 
 test('Select and from', () => {
-  const v = dd.action('t').select(user.id, user.name).from(user);
+  const v = dd.action('t')
+    .select(user.id, user.name)
+    .from(user)
+    .where`${user.id} = 1`;
   expect(v.name).toBe('t');
   expect(v.fromTable).toBe(user);
+  expect(v.whereExpr).not.toBeNull();
 });
