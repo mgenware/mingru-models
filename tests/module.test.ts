5375637c9f5b4ac7c1dea54bd06e2209cbcdee5f
Mgen <m@mgenware.com>
2018-11-07 01:48:08 +0800

    No need verify some members (covered in other tests)

--- tests/module.test.ts
+++ tests/module.test.ts
@@ -1,12 +1,6 @@
 const main = require('..');
 const fs = require('fs');
 
-describe('require this module', () => {
-  test('Verify module members', () => {
-    expect(typeof main.table).toBe('function');
-  });
-
-  test('Verify type definition files', () => {
-    expect(fs.statSync('./dist/main.d.ts').isFile()).toBeTruthy();
-  });
+test('Verify type definition files', () => {
+  expect(fs.statSync('./dist/main.d.ts').isFile()).toBeTruthy();
 });
