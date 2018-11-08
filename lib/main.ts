b9fb112e5d194aa5f8fed424e63826f03ded8353
Mgen <m@mgenware.com>
2018-11-09 00:55:26 +0800

    Fix file name case sensitivity causing test failure in linux

--- lib/main.ts
+++ lib/main.ts
@@ -13,7 +13,7 @@ export { char, int, notNull, pk, varChar } from './core/cols';
 
 export { default as SelectAction } from './actions/selectAction';
 export { default as UpdateAction } from './actions/updateAction';
-export { default as InsertAction } from './actions/InsertAction';
+export { default as InsertAction } from './actions/insertAction';
 export { default as action } from './actions/actionBuilder';
 export { default as input, InputParam } from './actions/input';
 export { default as sql, SQL, SQLParam, SQLElement } from './actions/sql';
