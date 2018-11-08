a363339c54373676ec2c061d46a1ae06110d8ba7
Mgen <m@mgenware.com>
2018-11-08 21:26:43 +0800

    More sql expr tests

--- tests/views/sql.test.ts
+++ tests/views/sql.test.ts
@@ -1,9 +1,8 @@
 import * as dd from '../..';
 import user from '../models/user';
 
-const SQL = dd.sql`${user.id} = 1 OR ${user.name} = ${dd.input(user.name)}`;
-
-test('Raw SQL', () => {
+test('SQL 1', () => {
+  const SQL = dd.sql`${user.id} = 1 OR ${user.name} = ${dd.input(user.name)}`;
   const equals = SQL.equalsTo(
     ['', ' = 1 OR ', ' = ', ''],
     [user.id, user.name, dd.input(user.name)],
@@ -11,6 +10,16 @@ test('Raw SQL', () => {
   expect(equals).toBe(true);
 });
 
+test('SQL 2', () => {
+  const SQL = dd.sql`START${user.id} = 1 OR ${user.name} = ${dd.input(user.name)}END`;
+  const equals = SQL.equalsTo(
+    ['START', ' = 1 OR ', ' = ', 'END'],
+    [user.id, user.name, dd.input(user.name)],
+  );
+  expect(equals).toBe(true);
+});
+
 test('Check type', () => {
+  const SQL = dd.sql`${user.id} = 1`;
   expect(SQL).toBeInstanceOf(dd.SQL);
 });
