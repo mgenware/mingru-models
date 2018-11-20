3d912aafaf75d7fe32f3dfd5aa7db419cfe9819a
Mgen <m@mgenware.com>
2018-11-20 23:47:32 +0800

    Asyncfy module.test.ts

--- tests/module.test.ts
+++ tests/module.test.ts
@@ -1,6 +1,7 @@
-const main = require('../');
-const fs = require('fs');
+import { promisify } from 'util';
+import { stat } from 'fs';
+const statAsync = promisify(stat);
 
-test('Verify type definition files', () => {
-  expect(fs.statSync('./dist/main.d.ts').isFile()).toBeTruthy();
+test('Verify type definition files', async () => {
+  expect((await statAsync('./dist/main.d.ts')).isFile()).toBeTruthy();
 });
