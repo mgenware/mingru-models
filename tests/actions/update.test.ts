a86ac04b6b907f9e17564e2b642906942f7c72fa
Mgen <m@mgenware.com>
2018-11-30 01:26:39 +0800

    Add tests for update without where

--- tests/actions/update.test.ts
+++ tests/actions/update.test.ts
@@ -17,3 +17,18 @@ test('Update', () => {
   expect(v.setters[1].column).toBe(user.follower_count);
   expect(v.setters[1].sql).not.toBeNull();
 });
+
+test('Update without where', () => {
+  const actions = dd.actions(user);
+  const v = actions.update('t')
+    .set(user.name, dd.sql`${dd.input(user.name)}`)
+    .set(user.follower_count, dd.sql`${user.follower_count} + 1`);
+
+  expect(v.name).toBe('UpdateT');
+  expect(v.table).toBe(user);
+  expect(v.setters.length).toBe(2);
+  expect(v.setters[0].column).toBe(user.name);
+  expect(v.setters[0].sql).not.toBeNull();
+  expect(v.setters[1].column).toBe(user.follower_count);
+  expect(v.setters[1].sql).not.toBeNull();
+});
