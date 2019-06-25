c18753870efa5d992f9a0115342e5b6ca99e1715
Mgen <m@mgenware.com>
2019-06-25 12:38:40 +0800

    Fix error thown when setters size is larger than column size

--- src/actions/insertAction.ts
+++ src/actions/insertAction.ts
@@ -14,16 +14,15 @@ export class InsertAction extends CoreUpdateAction {
 
     const setterCount = this.setters.size;
     // Number of columns = total count - number of auto_increment PKs
-    const cols = this.__table.__columns.length - this.__table.__pkAIs.length;
+    const colCount =
+      this.__table.__columns.length - this.__table.__pkAIs.length;
     if (
       !this.noColumnNumberCheck &&
       !this.autoSetter && // allSet mean all columns are set to inputs/defaults, check passed
-      setterCount !== cols
+      setterCount < colCount // setterCount can > colCount as you may set all columns but colCount = all - AI columns
     ) {
       throw new Error(
-        `You only set ${this.setters.size} of all ${
-          this.__table.__columns.length
-        } columns (not including AUTO_INCREMENT columns), you should set all columns or use 'unsafeInsert' to bypass this check`,
+        `You only set ${setterCount} of all ${colCount} columns (not including AUTO_INCREMENT columns), you should set all columns or use 'unsafeInsert' to bypass this check`,
       );
     }
   }
