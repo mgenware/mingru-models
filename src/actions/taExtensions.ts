f465d520ac403bf78dad2a44aff6baa1740bfb8e
Mgen <m@mgenware.com>
2019-07-20 22:43:01 +0800

    Modify action in place when chaining wrapped actions

--- src/actions/taExtensions.ts
+++ src/actions/taExtensions.ts
@@ -3,7 +3,7 @@ import { WrappedAction } from './wrappedAction';
 
 declare module './ta' {
   interface Action {
-    wrap(args: { [name: string]: string }): WrappedAction;
+    wrap(args: { [name: string]: any }): WrappedAction;
   }
 }
 
@@ -11,5 +11,14 @@ Action.prototype.wrap = function(args: {
   // tslint:disable-next-line no-any
   [name: string]: any;
 }): WrappedAction {
-  return new WrappedAction(this, args);
+  if (this.__table || this instanceof WrappedAction === false) {
+    return new WrappedAction(this, args);
+  }
+  // if this is a wrapped action && __table is not set, it means this action is not finalized and we can modify it in place
+  const action = this as WrappedAction;
+  action.args = {
+    ...action.args,
+    ...args,
+  };
+  return action;
 };
--- tests/actions/wrap.test.ts
+++ tests/actions/wrap.test.ts
@@ -18,3 +18,25 @@ test('WrappedAction', () => {
     id: '1',
   });
 });
+
+test('Chaining', () => {
+  class UserTA extends dd.TA {
+    t = dd.insert().setInputs();
+    t2 = this.t
+      .wrap({
+        name: 'a',
+        def_value: 'b',
+      })
+      .wrap({
+        def_value: 'c',
+        follower_count: 123,
+      });
+  }
+  const ta = dd.ta(user, UserTA);
+  const v = ta.t2;
+  expect(v.args).toEqual({
+    name: 'a',
+    def_value: 'c',
+    follower_count: 123,
+  });
+});
