c71106ef1751cc7cb9696dc2d0964e8af24a2ab3
Mgen <m@mgenware.com>
2020-05-27 02:17:46 +0800

    Fix failing tests due to change of SQLElement.toString

--- tests/core/sqlHelper.test.ts
+++ tests/core/sqlHelper.test.ts
@@ -7,13 +7,13 @@ const expect = assert.equal;
 it('and', () => {
   expect(
     mm.and(mm.sql`1`, user.id.isEqualToInput()).toString(),
-    '1 AND `id` = <id: [id]>',
+    'SQL(E(1, type = 0), E( AND , type = 0), E(Column(id, Table(user)), type = 1), E( = , type = 0), E(SQLVar(id, desc = Column(id, Table(user))), type = 2))',
   );
 });
 
 it('or', () => {
   expect(
     mm.or(mm.sql`1`, user.id.isEqualToInput()).toString(),
-    '1 OR `id` = <id: [id]>',
+    'SQL(E(1, type = 0), E( OR , type = 0), E(Column(id, Table(user)), type = 1), E( = , type = 0), E(SQLVar(id, desc = Column(id, Table(user))), type = 2))',
   );
 });
