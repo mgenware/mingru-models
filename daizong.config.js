00569be5d12a3770f7b1c186cf84bd3c2d74fe1e
Mgen <m@mgenware.com>
2020-05-27 02:51:39 +0800

    Switch to daizong

---
+++ daizong.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  clean: {
+    run: ['rimraf dist', 'rimraf dist_tests'],
+  },
+  lint: {
+    run: 'eslint --max-warnings 0 --ext .ts src/ tests/',
+  },
+  dev: {
+    run: ['#clean', 'tsc -b tests -w'],
+  },
+  t: {
+    run: 'mocha --require source-map-support/register dist_tests/**/*.test.js',
+  },
+  test: {
+    run: ['#clean', 'tsc -b tests', '#lint', '#t'],
+  },
+};
