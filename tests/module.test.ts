5eafb8ddbc07fc8a11b3346c5824e2ca6cfc2f4e
Mgen <m@mgenware.com>
2020-11-29 00:17:43 +0800

    Update to fsPromises in tests

--- tests/module.test.ts
+++ tests/module.test.ts
@@ -1,10 +1,6 @@
-import { promisify } from 'util';
-import { stat } from 'fs';
+import { promises as fsPromises } from 'fs';
 import * as assert from 'assert';
 
-const statAsync = promisify(stat);
-const eq = assert.equal;
-
 it('Verify type definition files', async () => {
-  eq((await statAsync('./dist/main.d.ts')).isFile(), true);
+  assert.ok((await fsPromises.stat('./dist/main.d.ts')).isFile());
 });
