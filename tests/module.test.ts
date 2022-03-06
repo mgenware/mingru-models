77f5f8dcae80b82a39cacb4a910da36a85a1c27e
Mgen <m@mgenware.com>
2022-03-06 19:50:47 +0800

    Move back to npm due to compat issues

--- tests/module.test.ts
+++ tests/module.test.ts
@@ -1,6 +1,7 @@
 import { promises as fsPromises } from 'fs';
 import * as assert from 'assert';
 
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
 it('Verify type definition files', async () => {
   assert.ok((await fsPromises.stat('./dist/main.d.ts')).isFile());
 });
