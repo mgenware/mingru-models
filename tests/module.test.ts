5f75f610298441593a75f5450d11c2ff0c07c934
Mgen <m@mgenware.com>
2022-03-07 17:02:18 +0800

    Revert problematic eslint-typescript versions

--- tests/module.test.ts
+++ tests/module.test.ts
@@ -1,7 +1,6 @@
 import { promises as fsPromises } from 'fs';
 import * as assert from 'assert';
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
 it('Verify type definition files', async () => {
   assert.ok((await fsPromises.stat('./dist/main.d.ts')).isFile());
 });
